650770222f60e8c1b4dc4685393d15cbca129cab
Aaron <aaronjacobsy@gmail.com>
2022-03-31 20:41:01 +0800

    main page front end done

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div id="autodata">
+</div>
+
+<!--<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Driver</th>
+                <th>Location</th>
+                <th>Date</th>
+                <th>Time</th>
+                <th>Drowsiness Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td scope="row"></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td scope="row"></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+    
+</div> -->
+@endsection
