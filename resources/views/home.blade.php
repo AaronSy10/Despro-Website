c54a06500f513b790bf5c2832a017c11b5b505e7
Aaron <aaronjacobsy@gmail.com>
2022-04-01 02:14:30 +0800

    table front end tentative

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,34 +3,4 @@
 @section('content')
 @widget('DriverData')
 
-<!--<div class="container">
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Driver</th>
-                <th>Location</th>
-                <th>Date</th>
-                <th>Time</th>
-                <th>Drowsiness Status</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td scope="row"></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td scope="row"></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-        </tbody>
-    </table>
-    
-</div> -->
 @endsection
